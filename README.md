6f42800a95b468d0d152842e5bb6ec7e2e9ddf41
Taehoon <hoonblizz@gmail.com>
2020-01-13 09:06:41 -0500

    Dart package install

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 소스: [Learn Flutter & Dart to Build iOS & Android Apps](https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps)
 <br>
 
+## 유용한 사이트 모음
+[Dart 패키지 모음](https://pub.dev/)
+
 ## 목차
 | Title | Desc | 
 | :--- | :---:  | 
@@ -128,7 +131,81 @@ body: Column(
 예를들어, `mainAxisAlignment`, `crossAxisAlignment` 를 이용해서 alignment 규칙을 만들수 있다. <br><br>
 Javascript 의 `flex` 를 알고있다면 익숙할 내용이다. <br>
 
+## Widget Styling
+어떤 위젯은 내가 하고싶어하는 스타일링이 안될수도 있다. <br>
+예를들어, `Text` 위젯 백그라운드에 색을 넣고 싶다랄지, 공간을 넣고 싶다랄지...<br>
+그런 경우엔 해당 위젯을 Refactoring 해서 다른 위젯으로 감싸서 해결하자.
+```dart
+...
+Container(
+  margin: EdgeInsets.symmetric(
+    vertical: 10,
+    horizontal: 15,
+  ),
+  decoration: BoxDecoration(
+      border: Border.all(
+    color: Colors.black,
+    width: 2,
+  )),
+  padding: EdgeInsets.all(10),
+  child: Text(
+    e.amount.toString(),
+  ),
+),
+...
+```
 
+## String Interpolation
+예를들어, <br>
+```dart
+...
+child: Text(
+  e.amount.toString(),
+  style: TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.purple,
+  ),
+),
+...
+```
+`e.amount.toString()` 이 물건값을 가리키는 값이라 할때, 여기에 달라 사인을 붙여보고 싶다.<br>
+달라 사인의 문제는, 스트링을 붙여넣는 신택스로 `${}` 이게 쓰이기 때문에, 쉽게 달라 사인을 쓸수가 없는것. <br>
+즉, 다음의 두줄이 같은 기능을 한다. <br>
+```dart
+'A: ' + e.amount.toString()
+'A: ${e.amount.toString()}'
+```
+해답은 `\` 백슬래쉬를 달라사인 앞에 넣는것. (다른 프로그래밍 언어도 비슷하다)<br>
+```dart
+'\$ ${e.amount.toString()}'
+```
 
+## Dart Packages install
+[Pub.dev](https://pub.dev/) 에서 `intl` 이라는 패키지를 설치한다. <br>
+이건 로컬라이징이나, Date 를 좀더 나은 포멧으로 바꿔주는 유용한 패키지이다. <br>
+Puv.dev 에서 intl 을 찾아서, install 방법을 보면, <br>
+```
+Add this to your package's pubspec.yaml file:
+dependencies:
+  intl: ^0.16.1
+```
+이런식으로 쓰여져 있다. 말 그대로, 저 디펜던시를 복사해서, `pubspec.yaml` 에 붙여넣기 하자. <br>
+yaml 에서는 indentation이 매우 중요하므로, 디펜던시를 붙여넣을때, 꼭 flutter 와 같은 레벨에서 붙여넣자. <br>
+이게 무슨 소리냐 하면, 기본적으로 pubspec.yaml 을 보면, <br>
+```
+...
+dependencies:
+  flutter:
+    sdk: flutter
 
-
+...
+```
+이렇게 되어있는데, flutter 와 같은 레벨의 인덴테이션에서 새 패키지를 넣어준다. <br>
+```
+dependencies:
+  flutter:
+    sdk: flutter
+  intl: ^0.16.1
+```
+잘 넣어줬으면, VSCode 같은 경우에는 자동으로 설치를 해준다.
+해당 패키지에 대한 자세한 설명은, [API reference](https://pub.dev/documentation/intl/latest/intl/intl-library.html) 를 참조하도록 한다. 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/semantics.dart';
+import 'package:intl/intl.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,6 +15,22 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  // List of <Transaction>
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'New Pencil',
+      amount: 7.45,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +38,10 @@ class MyHomePage extends StatelessWidget {
           title: Text('Homepage text'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            // Chart 컨테이너
             Container(
               width: double.infinity,
               child: Card(
@@ -32,9 +50,61 @@ class MyHomePage extends StatelessWidget {
                 color: Colors.blue,
               ),
             ),
+            // 유저 인풋 컨테이너
             Card(
-              color: Colors.green,
-              child: Text('List of transaction!'),
+              child: Column(
+                children: <Widget>[
+                  TextField(),
+                ],
+              ),
+            ),
+            // 트랜젝션 컨테이너
+            Column(
+              children: transactions.map((e) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        width: 90,
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        )),
+                        padding: EdgeInsets.all(10),
+                        alignment: Alignment.center,
+                        child: Text(
+                          '\$ ${e.amount.toString()}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            e.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(e.date),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
             ),
           ],
         ));
